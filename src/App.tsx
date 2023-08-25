dd86cb812e13b87c8653e21188b80ef5e870f3c7
Dmitry48 <Dima_1996.33@mail.ru>
2023-08-25 13:54:10 +0700

    changes 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,46 @@ import './App.css';
 import {Header} from './site/Header';
 import {Body} from './site/Body'
 import {Footer} from './site/Footer'
+import {MouseEvent} from 'react';
+import {Button} from './components/Button';
+
 
 function App() {
+    // const myFirstSubscruber = (event:MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Hello Dmitry')
+    // };
+    //
+    // const mySecondSubscruber = () => {
+    //     console.log('Hello Ivan')
+    // }
+    //
+    // const onClickHandler=(name:string) => {
+    //     console.log(name)
+    // }
+const Button1Foo =(subscruber:string, age: number) => {
+    console.log(subscruber, age)
+
+}
+    const Button2Foo =(subscruber:string) => {
+        console.log(subscruber)
+
+    }
+
     return (
-        <>
-        <Header titleForHeader={'NEW BODY'}/>
-        <Body titleForBody={'NEW BODY'}/>
-        <Footer titleForFooter={'NEW FOOTER'}/>
-        </>
+
+
+
+        <div>
+            <Header titleForHeader={'NEW BODY'}/>
+            <Body titleForBody={'NEW BODY'}/>
+            <Footer titleForFooter={'NEW FOOTER'}/>
+            {/*<button onClick={(event:MouseEvent<HTMLButtonElement>) =>onClickHandler('Dmitry')}>MyYouTubeChanel-1</button>*/}
+            {/*<button onClick={(event:MouseEvent<HTMLButtonElement>) =>onClickHandler('Ivan')}>MyYouTubeChanel-2</button>*/}
+            {/*<button>MyYouTubeChanel-1</button>*/}
+            {/*<button>MyYouTubeChanel-2</button>*/}
+            <Button name={'MyYouTubeChanel-1'} callBack={()=>Button1Foo('I am Dmitry', 21)}/>
+            <Button name={'MyYouTubeChanel-2'} callBack={()=>Button2Foo('I am Ivan')}/>
+        </div>
     );
 }
 
